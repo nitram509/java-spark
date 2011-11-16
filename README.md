7259c0b64a1ce7f76eaee87bc66ca2bab9415599
Yannik Stein <yannik.stein@fu-berlin.de>
2011-11-16 20:39:22 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# java-spark
+
+## Usage
+`javac Spark.java && java Spark 1 2 3 4 5`
+
+## Hint
+Spark.java converts chars to the corresponding int value.
+For example, this works too:
+
+```
+java Spark j a v a - s p a r k
+▆▅▇▅▁▆▆▅▆▆
+```
+## See also
+[holman/spark](https://github.com/holman/spark)
---
+++ Spark.java
@@ -0,0 +1,44 @@
+public class Spark {
+
+    public static void main(String[] args) {
+        System.out.println(spark(args));
+    }
+
+    public static String spark(String... input) {
+        double[] numbers = new double[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            try {
+                numbers[i] = Double.parseDouble(input[i]);
+            } catch (NumberFormatException e) {
+                numbers[i] = input[i].charAt(0);
+            }
+        }
+
+        return spark(numbers);
+    }
+
+    public static String spark(double[] input) {
+        double min = Double.POSITIVE_INFINITY;
+        double max = Double.NEGATIVE_INFINITY;
+
+        for (double d : input) {
+            if (d < min) {
+                min = d;
+            }
+            if (d > max) {
+                max = d;
+            }
+        }
+        double intervalSize = max - min;
+
+        StringBuilder sb = new StringBuilder(input.length);
+        String sparks = "▁▂▃▄▅▆▇";
+        for (double d : input) {
+            int sparkIndex = (int) ((d - min) / intervalSize * (sparks.length() - 1));
+            sb.append(sparks.charAt(sparkIndex));
+        }
+
+        return sb.toString();
+    }
+}
